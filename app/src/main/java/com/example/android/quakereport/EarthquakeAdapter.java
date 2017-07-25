0bf7c998d068570986db466cb3a40530923fcd85
Lewei.H <hexxx373@umn.edu>
2017-07-25 16:06:00 -0500

    Add colored background circle to the magnitude textview

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+
+import static com.example.android.quakereport.R.id.mag;
 
 /**
  * Created by usa19 on 7/20/2017.
@@ -51,7 +55,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         Date dateObject = new Date(timeInMs);
 
         //populate the mag field
-        TextView magnitude = (TextView) listItemView.findViewById(R.id.mag);
+        TextView magnitude = (TextView) listItemView.findViewById(mag);
         magnitude.setText(formatMagnitude(currentEarthquake.getMagnitude()));
         //populate the primary location field
         TextView location1 = (TextView) listItemView.findViewById(R.id.primaryLoc);
@@ -68,7 +72,58 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         String time = formatTime(dateObject);
         timeText.setText(time);
 
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitude.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+
         return listItemView;
+    }
+
+    private int getMagnitudeColor(Double magnitude) {
+        int magColorID;
+        //floor will find the closest integer less than the decimal value
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magColorID = R.color.magnitude1;
+                break;
+            case 2:
+                magColorID = R.color.magnitude2;
+                break;
+            case 3:
+                magColorID = R.color.magnitude3;
+                break;
+            case 4:
+                magColorID = R.color.magnitude4;
+                break;
+            case 5:
+                magColorID = R.color.magnitude5;
+                break;
+            case 6:
+                magColorID = R.color.magnitude6;
+                break;
+            case 7:
+                magColorID = R.color.magnitude7;
+                break;
+            case 8:
+                magColorID = R.color.magnitude8;
+                break;
+            case 9:
+                magColorID = R.color.magnitude9;
+                break;
+            default:
+                magColorID = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magColorID);
     }
 
     private String formatDate(Date dateObject) {
